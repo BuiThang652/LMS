f23615c03e1f28ed041483d8f9d962a4d6ceeca6
BuiDucThanggg <buiducthang652@gmail.com>
2022-06-29 11:34:36 +0700

    full html

--- assets/js/jquery.js
+++ assets/js/jquery.js
@@ -1,8 +1,8 @@
 // JSON
 // tài khoản 1
-username = '705102020';
-email = 'buiducthang652@gmail.com';
-password = '123';
+username = "705102020";
+email = "buiducthang652@gmail.com";
+password = "123";
 var user = {
   username: username,
   email: email,
@@ -27,4 +27,37 @@ function login(e) {
   }
 }
 
-// thay đổi tên đăng nhập
+// Ngày hiện tại
+var today = new Date();
+var date = today.getDate()+'-'+(today.getMonth()+1)+'-'+today.getFullYear();
+var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+var dateTime = date+' '+time;
+
+var date = new Date();
+var current_day = date.getDay();
+var day_name = '';
+switch (current_day) {
+case 0:
+    day_name = "Chủ nhật";
+    break;
+case 1:
+    day_name = "Thứ hai";
+    break;
+case 2:
+    day_name = "Thứ ba";
+    break;
+case 3:
+    day_name = "Thứ tư";
+    break;
+case 4:
+    day_name = "Thứ năm";
+    break;
+case 5:
+    day_name = "Thứ sau";
+    break;
+case 6:
+    day_name = "Thứ bảy";
+}
+
+document.getElementById("today").innerHTML = dateTime;
+document.getElementById("rank").innerHTML = day_name;
